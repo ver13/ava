fc482d8a0877abc4d64279aed63606617276ecc2
Valentin Encinas <valentin.encinas.rojas@gmail.com>
2020-04-14 10:52:16 +0200

    .

--- pkg/common/commands/versionCmd.go
+++ pkg/common/commands/versionCmd.go
@@ -15,21 +15,13 @@ var VersionCmd = &cobra.Command{
 }
 
 func versionCmdF(command *cobra.Command, args []string) error {
-	l := versionAVA.GetInstance()
+	l, _ := versionAVA.GetInstance()
 
 	_, _ = CommandPrintln("Name: " + l.Name)
 	_, _ = CommandPrintln("ServerName: " + l.ServerName)
 	_, _ = CommandPrintln("ClientName: " + l.ClientName)
 
 	_, _ = CommandPrintln("Version: " + l.SemanticVersion.String())
-
-	b := len(l.SemanticVersion.Build)
-	for i := 0; i < b; i++ {
-		_, _ = CommandPrintln("Build Number: " + l.SemanticVersion.Build[i].BuildNumber)
-		_, _ = CommandPrintln("Build Date: " + l.SemanticVersion.Build[i].BuildDate.String())
-		_, _ = CommandPrintln("Build Hash: " + l.SemanticVersion.Build[i].BuildHash)
-		_, _ = CommandPrintln("Build tags: " + l.SemanticVersion.Build[i].BuildTags)
-	}
 	_, _ = CommandPrintln("Git commit: " + l.GitCommit)
 	_, _ = CommandPrintln("GoVersion: " + l.GoVersion)
 	return nil
